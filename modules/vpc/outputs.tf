052218e07d5c712fb31a827a645e2acb8e3d1f7e
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2023-05-02 22:47:25 +0200

    created vpc

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,34 @@
+# export region
+output "region" {
+  value = var.region
+}
+
+# export project name
+output "project_name" {
+  value = var.project_name
+}
+
+# export vpc id
+output "vpc_id" {
+  value = aws_vpc.vpc.id
+}
+
+# export the internet gateway
+output "internet_gateway" {
+  value = aws_internet_gateway.internet_gateway
+}
+
+# export the public subnet az1 id
+output "public_subnet_az1_id" {
+  value = aws_subnet.public_subnet_az1.id
+}
+
+# export the private subnet az1 id
+output "private_subnet_az1_id" {
+  value = aws_subnet.private_subnet_az1.id
+}
+
+# export the private subnet az2 id
+output "private_subnet_az2_id" {
+  value = aws_subnet.private_subnet_az2.id
+}
