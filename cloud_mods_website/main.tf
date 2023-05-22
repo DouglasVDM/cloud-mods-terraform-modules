4f742557961e312c5d014b4a34195585604bdebe
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2023-05-22 20:45:13 +0200

    added project dir to reference modules

---
+++ cloud_mods_website/main.tf
@@ -0,0 +1,37 @@
+# configure aws provider
+provider "aws" {
+  region  = var.region
+  profile = "terraform-user"
+}
+
+# create vpc
+module "vpc" {
+  source                   = "../modules/vpc"
+  region                   = var.region
+  project_name             = var.project_name
+  vpc_cidr                 = var.vpc_cidr
+  public_subnet_az1_cidr   = var.public_subnet_az1_cidr
+  public_subnet_az2_cidr   = var.public_subnet_az2_cidr
+  private_subnet_az1_cidr  = var.private_subnet_az1_cidr
+  private_subnet_az2_cidr  = var.private_subnet_az2_cidr
+  app_instance_id          = module.ec2.app_instance_id
+  public_security_group_id = module.security_groups.public_security_group_id
+  key_name                 = var.key_name
+}
+
+# create security_groups
+module "security_groups" {
+  source                 = "../modules/security_groups"
+  project_name           = var.project_name
+  vpc_id                 = module.vpc.vpc_id
+  public_subnet_az1_cidr = var.public_subnet_az1_cidr
+}
+
+# create ec2
+module "ec2" {
+  source                   = "../modules/ec2"
+  project_name             = var.project_name
+  public_subnet_az1_id     = module.vpc.public_subnet_az1_id
+  public_security_group_id = module.security_groups.public_security_group_id
+  key_name                 = var.key_name
+}
