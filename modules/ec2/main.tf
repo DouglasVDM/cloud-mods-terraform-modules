12a75b571b209cf3fd478e4326da998fc46da1cb
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2023-05-22 20:49:21 +0200

    removed network interface

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,7 +1,7 @@
 # 9. Create Ubuntu server and install/enable nginx
 data "aws_availability_zones" "available_zones" {}
 
-resource "aws_instance" "app-instance" {
+resource "aws_instance" "app_instance" {
   ami                         = var.ami
   instance_type               = var.instance_type
   availability_zone           = data.aws_availability_zones.available_zones.names[0]
@@ -10,26 +10,10 @@ resource "aws_instance" "app-instance" {
   vpc_security_group_ids      = [var.public_security_group_id]
   associate_public_ip_address = var.associate_public_ip_address
 
-  user_data = <<-EOF
-                #!/bin/bash
-                echo "update packages"
-                sudo apt update -y
-                echo "install nginx"
-                sudo apt install nginx -y
-                sudo systemctl enable nginx
-                EOF
+  user_data = file("../modules/ec2/user_data.sh")
+
   tags = {
-    Name = "${var.project_name}-app-instance"
+    Name = "${var.project_name}_app_instance"
   }
 }
 
-resource "aws_network_interface" "network-interface" {
-  subnet_id                   = var.public_subnet_az1_id
-  private_ips     = ["10.11.1.50"]
-  security_groups = [var.public_security_group_id]
-
-  attachment {
-    instance     = aws_instance.app-instance.id
-    device_index = 1
-  }
-}
