e5ba775776e10c1307a17b503a136018cde119c8
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2023-05-22 20:46:14 +0200

    added project dir variables

---
+++ cloud_mods_website/variables.tf
@@ -0,0 +1,14 @@
+# environment variables
+variable "region" {}
+variable "project_name" {}
+
+# vpc variables
+variable "vpc_cidr" {}
+variable "public_subnet_az1_cidr" {}
+variable "public_subnet_az2_cidr" {}
+variable "private_subnet_az1_cidr" {}
+variable "private_subnet_az2_cidr" {}
+
+variable "app_instance_id" {}
+variable "key_name" {}
+# variable "public_security_group_id" {}
