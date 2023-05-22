4af27f415c40460d560b63932b3cd9da5c14629e
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2023-05-22 20:36:30 +0200

    added s3 backend dynamodb table

---
+++ cloud_mods_website/backend.tf
@@ -0,0 +1,12 @@
+# store the terraform state file in s3
+terraform {
+  backend "s3" {
+    bucket  = "cloud-mods-modules-remote-state"
+    key     = "cloud-mods-website.tfstate"
+    region  = "us-east-1"
+    profile = "terraform-user"
+
+    dynamodb_table = "cloud-mods-modules-remote-state-locks"
+    encrypt        = true
+  }
+}
