21f19c8ba9d0f4566bbb465b1ed148ab6f9d3514
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2023-05-22 20:28:24 +0200

    added ec2 variables

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -8,3 +8,9 @@ variable "public_subnet_az1_cidr" {}
 variable "public_subnet_az2_cidr" {}
 variable "private_subnet_az1_cidr" {}
 variable "private_subnet_az2_cidr" {}
+
+variable "app_instance_id" {}
+variable "public_security_group_id" {}
+
+# ec2 variables
+variable "key_name" {}
